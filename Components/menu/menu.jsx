af4a32d227c390944aaf45dbca5fa9762c6cec3d
MariaLerato <fenyane1999@gmail.com>
2021-10-25 13:28:58 +0200

    fixed bugs

--- Components/menu/menu.jsx
+++ Components/menu/menu.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Register from '../registerComponents/register'
-import HomeScreen from '../screens/HomeScreen'
+import HomeScreen from '../screens/home'
 
 const Menu = () =>{
     const Stack = createNativeStackNavigator()
-
     return(
         <NavigationContainer>
-            <Stack.Navigator initialRouteName={'Home'} screenOptions={{headerShown:false}}>
-            <Stack.Screen name={'HomeScreen'} component={HomeScreen} />
+            <Stack.Navigator initialRouteName={'Home'}>
+            <Stack.Screen name={'home'} component={HomeScreen} />
             <Stack.Screen name={'register'} component={Register} />
             </Stack.Navigator>
 
