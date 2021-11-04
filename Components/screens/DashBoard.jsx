ef40cd121a04bc0f923d81e0dfadd96d22af4d8e
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-11-04 15:58:23 +0200

    design

---
+++ Components/screens/DashBoard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, Text, SafeAreaView,FlatList } from 'react-native'
+import HomeCard from '../Card/HomeCard'
+
+const DashBoard = () => {
+
+    const services = [{ name: "Material", icon: "question-circle", type: "font-awesome" }, { name: "Lesson", icon: "book", type: "font-awesome" },{name:"Q&A",icon:"question-circle",type:"font-awesome"},{name:"Papers",icon:"file-text",type:"font-awesome"}]
+
+
+    return (
+        <SafeAreaView style={{ justifyContent: 'center', alignItems: 'center',}}>
+            <FlatList
+                data={services}
+                renderItem={({item})=>(
+                    <HomeCard data={item}/>
+                )}
+                horizontal={false}
+                numColumns={2}
+            />
+           
+        </SafeAreaView>
+    )
+}
+
+export default DashBoard
