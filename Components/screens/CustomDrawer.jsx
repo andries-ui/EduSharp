6e831367aac2aae56518125c63a30f233f5be013
stuurdean <68418793+stuurdean@users.noreply.github.com>
2022-01-17 15:44:09 +0200

    commit

---
+++ Components/screens/CustomDrawer.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {View, Text,
+    ImageBackground, Image,
+    StyleSheet, TouchableOpacity} from 'react-native';
+import { ListItem, Header,Button } from 'react-native-elements';
+import {DrawerContentScrollView, DrawerItemList} from '@react-navigation/drawer'
+import Icon from 'react-native-vector-icons/Ionicons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+
+const CustomDrawer = (props) =>{
+    return(
+<View style={{flex:1}}>
+        
+        <DrawerContentScrollView {...props} >
+        
+        <View style={styles.bottomView}>
+
+        <View style={{padding:40}} >
+<Image source={require('../images/Use.png')} style={{height:80, width:80,margin:40, borderRadius:40 }}/> 
+             <Text style={{color:'#fff', fontSize:18, fontWeight:'bold', margin:20,bottom:20}}>Leah Makgatho</Text> 
+        </View>
+        </View>
+
+             
+                  {/* <ImageBackground source={require('../images/background.jpg')} style={{width:200, height:200}}>  */}
+                  {/* <Image source={require('../images/Use.png')} style={{height:90, width:80,margin:40, borderRadius:40, marginBottom:10  }}/> 
+             <Text style={{color:'red', fontSize:18, margin:40, fontWeight:'bold'}}>Leah Makgatho</Text> 
+        </ImageBackground>  */}
+
+        
+                
+                <DrawerItemList {...props}/>
+        </DrawerContentScrollView>
+
+        
+
+
+ <View style={{flexDirection:'row',  padding:20, borderTopWidth:1, borderTopColor:'gray'}}>
+    <TouchableOpacity onPress={()=>{}} >
+    <View style={{left:180}}>
+    <FontAwesome5 name="power-off" size={20} color="red"/>
+    </View>
+
+    <View  style={{flexDirection:'row', justifyContent:'space-between'}}>
+    <Text>T's & Cs</Text> 
+
+    <Text style={{left:110}} >Log-out</Text>
+   
+    </View>
+    
+    </TouchableOpacity>
+
+</View> 
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    inner: {
+        width:'193px',
+        height:'80px',
+        top:'350px',
+
+    },
+
+    bottomView:{
+  flex:1.5,
+  backgroundColor:'#4B7BE8',
+  bottom:50,
+  borderBottomRightRadius:60,
+  height:250
+    },
+    textdes:{
+        fontWeight:'bold',
+    paddingBottom:10,
+        color:'#0b1674',
+ }
+
+
+    
+})
+
+export default CustomDrawer
---
+++ Components/screens/HomeScreen.jsx
@@ -0,0 +1,148 @@
+import * as React from 'react';
+import { Button, View, Text,SafeAreaView,Image, TouchableOpacity } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { Icon } from 'react-native-elements';
+import Slideshow from 'react-native-image-slider-show';
+//import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Card } from 'react-native-paper';
+//import Icon from 'react-native-vector-icons/Ionicons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+
+const HomeScreen = ({ navigation })=> {
+   
+    return (
+    <>
+    <SafeAreaView>
+            <View style ={{top:10, margin:30, borderTopRadius: 5, borderTopRightRadius: 5 }}>
+         <Slideshow 
+         dataSource={[
+           { url:'http://placeimg.com/640/480/any' },
+          { url:'http://placeimg.com/640/480/any' },
+           { url:'http://placeimg.com/640/480/any' }
+       ]}
+      // source={require('../images/Use.png')}
+
+      />
+
+    </View>
+
+    <View style={{flex:1, display:'flex',
+ flexDirection: 'row', justifyContent:'space-between', paddingHorizontal:20}}>
+
+         
+         
+<Card style={{width:'43%',height:'125%', paddingHorizontal:30, borderRadius:10}} >
+         <TouchableOpacity  onPress={()=>navigation.navigate('Material')}>
+            <View style={{marginTop:20, paddingHorizontal:10}}>
+            <FontAwesome5 name="folder-open" size={30} color="#4B7BE8"/>
+           
+           </View>
+           <View>         
+                 <Text style={{fontWeight:'bold'}}>Material</Text>
+            </View>
+           </TouchableOpacity>
+            </Card>
+ 
+
+            <Card style={{width:'43%',height:'125%', paddingHorizontal:30, borderRadius:10}} >
+         <TouchableOpacity >
+            <View style={{marginTop:20, }}>
+               <View style={{paddingHorizontal:10}}>
+               <FontAwesome5 name="edit" size={30} color="#4B7BE8"/>
+
+               </View>
+           <Text style={{fontWeight:'bold'}}>Lessons</Text>
+           </View>
+           </TouchableOpacity>
+            </Card>
+
+      {/* <Card style={{width:150,height:150, margin:30}}>
+               <View style={{ top:40, left:60}}>
+            <FontAwesome5 name="folder-open" size={50} color="#4B7BE8" margin={20}/>
+            </View>
+
+            <Text style={{fontSize:20, fontWeight:'normal', textAlign:'center', top:60}}>Materials</Text>         
+
+
+           </Card>
+
+<Card style={{width:150,height:150, margin:30}}>
+              
+              
+               <View style={{ top:40, left:60}}>
+            <FontAwesome5 name="edit" size={50} color="#4B7BE8" margin={20}/>
+            </View>
+
+            <Text style={{fontSize:20, fontWeight:'normal', textAlign:'center', top:60}}>Lessons</Text>         
+
+
+           </Card>  */}
+
+           
+
+           
+</View>
+
+<View style={{flex:1, display:'flex',
+ flexDirection: 'row', justifyContent:'space-between', paddingHorizontal:20, paddingTop:60}}>
+
+
+<Card style={{width:'43%',height:'125%', paddingHorizontal:30, borderRadius:10}} >
+         <TouchableOpacity  onPress={()=>navigation.navigate('QList')}>
+            <View style={{marginTop:20, paddingHorizontal:10}}>
+            <FontAwesome5 name="file" size={30} color="#4B7BE8"/>
+           </View>
+           <View >
+           <Text style={{fontWeight:'bold'}}>Q'Ans</Text>
+
+           </View>
+           </TouchableOpacity>
+            </Card>
+ 
+
+            <Card style={{width:'43%',height:'125%', paddingHorizontal:30, borderRadius:10}} >
+         <TouchableOpacity >
+            <View style={{marginTop:20,paddingHorizontal:10}}>
+            <FontAwesome5 name="copy" size={30} color="#4B7BE8"/>
+           </View>
+
+           <View >
+           <Text style={{fontWeight:'bold'}}>Papers</Text>
+
+           </View>
+           </TouchableOpacity>
+            </Card>
+ 
+{/* <Card style={{width:150,height:150, margin:30}}>
+               <View style={{ top:40, left:60}}>
+            <FontAwesome5 name="file" size={50} color="#4B7BE8" margin={20}/>
+            </View>
+
+            <Text style={{fontSize:20, fontWeight:'normal',textAlign:'center', top:60}}>Q's A's</Text>         
+
+
+           </Card> 
+          
+
+           <Card style={{width:150,height:150, margin:30}}>
+               <View style={{ top:40, left:60}}>
+            <FontAwesome5 name="copy" size={50} color="#4B7BE8" margin={20}/>
+            </View>
+
+            <Text style={{fontSize:20, fontWeight:'normal', textAlign:'center', top:60}}>Papers</Text>         
+
+
+           </Card>  */}
+
+
+
+      </View>
+</SafeAreaView>
+
+    </>
+    )
+  }
+
+  export default HomeScreen    
