91508caffcb6125d982011fbe769a7d5e7e72fdd
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2022-01-19 10:00:32 +0200

    backend firebase

---
+++ BackendFirebase/Firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from 'firebase'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDh51b15XlbShMIeETE3iroRxMpaPaUM5M",
+  authDomain: "edusharp-de629.firebaseapp.com",
+  projectId: "edusharp-de629",
+  storageBucket: "edusharp-de629.appspot.com",
+  messagingSenderId: "257078871465",
+  appId: "1:257078871465:web:b99db41c817dc5f85ff0b8",
+  measurementId: "G-N8ZZCGZCG1"
+};
+
+// Initialize Firebase
+let app;
+if(firebase.apps.length===0){
+    app=firebase.initializeApp(firebaseConfig)
+}else{
+    app=firebase.app();
+}
+
+const auth=firebase.auth();
+const database=firebase.database();
+
+export {auth,database};
---
+++ BackendFirebase/FirebaseApi.js
@@ -0,0 +1,80 @@
+import { auth } from "./Firebase";
+
+class EduSharp {
+  async SignUp(data) {
+    if (!data.email || !data.password || !data.username) {
+      return JSON.stringify({
+        status: "Error",
+        message: "Please enter all required fields!",
+      });
+    }
+    await auth
+      .createUserWithEmailandPassword(data.email, data.password)
+      .then((userCredentials) => {
+        //app user to database
+        const user = userCredentials.user;
+      })
+      .catch((error) => {
+        return JSON.stringify({ status: "Error", message: error.message });
+      });
+  }
+  async SignIn(data) {
+    if (!data.email || !data.password) {
+      return JSON.stringify({
+        status: "Error",
+        message: "Please enter all required fields!",
+      });
+    }
+    await auth
+      .signInWithEmailAndPassword(data.email, data.password)
+      .then((userCredentials) => {
+        return JSON.stringify({
+          status: "success",
+          message: "sign in successful",
+          data: userCredentials.user,
+        });
+      })
+      .catch((error) => {
+        return JSON.stringify({ status: "Error", message: error.message });
+      });
+  }
+  async SignOut(navigation, location) {
+    await auth
+      .signOut()
+      .then(() => {
+        navigation.replace(location);
+      })
+      .catch((error) => {
+        return JSON.stringify({ status: "Error", message: error.message });
+      });
+  }
+  async authState() {
+    await auth.onAuthStateChanged((user) => {
+      if (user) {
+        return JSON.stringify({ status: "success", message: "user logged in" });
+      } else {
+        return JSON.stringify({
+          status: "Error",
+          message: "user not logged in",
+        });
+      }
+    });
+  }
+  async currentUser() {
+    const user = await auth.currentUser;
+    if (user) {
+      return JSON.stringify({
+        status: "success",
+        message: "user logged in",
+        user,
+      });
+    } else {
+      return JSON.stringify({
+        status: "Error",
+        message: "user not logged in",
+      });
+    }
+  }
+}
+
+export default new EduSharp();
